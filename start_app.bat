1ead84b4d3f565c3055cf517b338d535fb97dc3f
sumankaur56318-spec <sumankaur56318@gmail.com>
2026-09-25 11:00:54 +0530

    Add start_app.bat launcher

---
+++ start_app.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d "%~dp0"
+if exist .venv\Scripts\activate.bat (
+    call .venv\Scripts\activate.bat
+) else if exist venv\Scripts\activate.bat (
+    call venv\Scripts\activate.bat
+)
+python app.py
+pause
